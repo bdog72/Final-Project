bc1892a1764e6a4826b8af074d7cbfefcdd829c1
Brian Bycynski <high5bri@hotmail.com>
2017-01-29 12:26:19 -0500

    Got some Bootstrap working

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react'
 import { Navbar, PageHeader } from 'react-bootstrap'
 import Thumbnails from './Thumbnails'
-
 export default class Main extends Component {
   render () {
     return <div>
       <PageHeader className='head'><p>Rest-au-rant</p></PageHeader>
       <Navbar className='nav' />
-      {/* <Thumbnails /> */}
       <Thumbnails />
     </div>
   }
