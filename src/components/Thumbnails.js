c85fbbd9f739078aff5142419678c570391ed7d1
Brian Bycynski <high5bri@hotmail.com>
2017-01-29 10:04:04 -0500

    Got some Bootstrap working

--- src/components/Thumbnails.js
+++ src/components/Thumbnails.js
@@ -6,16 +6,16 @@ export default class Thumbnails extends Component {
       <Grid>
         <Row>
           <Col xs={6} md={3}>
-            <Thumbnail href='#' alt='171x180' src='/assets/thumbnail.png' />
+            <Thumbnail src='http://mississippiseafoodtrail.com/img/dinner-plate.png' width='200' height='200' />
           </Col>
           <Col xs={6} md={3}>
-            <Thumbnail href='#' alt='171x180' src='/assets/thumbnail.png' />
+            <Thumbnail src='http://www.addictedtogrilling.com/sites/default/files/burger_works_386x311.jpg' width='200' height='200' />
           </Col>
           <Col xs={6} md={3}>
-            <Thumbnail href='#' alt='171x180' src='/assets/thumbnail.png' />
+            <Thumbnail src='http://foodnetwork.sndimg.com/content/dam/images/food/fullset/2012/12/20/0/FNM_010113-Chinese-Barbecue-Wings-Recipe_s4x3.jpg.rend.sniipadlarge.jpeg' width='200' height='200' />
           </Col>
           <Col xs={6} md={3}>
-            <Thumbnail href='#' alt='171x180' src='/assets/thumbnail.png' />
+            <Thumbnail src='http://www.cicis.com/media/1138/pizza_trad_pepperoni.png' width='200' height='200' />
           </Col>
         </Row>
       </Grid>
