221660c66b537b16f3fe22de97a7b56419f93055
Brian Bycynski <high5bri@hotmail.com>
2017-02-07 17:27:13 -0500

    minor changes

--- src/components/Practice.js
+++ src/components/Practice.js
@@ -4,7 +4,7 @@ export default class Practice extends Component {
 
   render () {
     return <div>
-      <h1 className='foodChoices'>Hello</h1>
+      <h1 className='foodChoices'>Practice</h1>
     </div>
   }
 }
