d68fda48b78c53c136d1917c187d84c7edfefc15
Brian Bycynski <high5bri@hotmail.com>
2017-02-08 07:14:32 -0500

    minor changes

--- src/components/Pizza.js
+++ src/components/Pizza.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react'
+import { Jumbotron } from 'react-bootstrap'
 
 export default class Pizza extends Component {
   render () {
     return <div>
       <h1 className='heads1'>Pizza</h1>
+      <main className='mainB'>
+        <Jumbotron className='jumbo1' />
+      </main>
     </div>
   }
 }
--- src/components/Wings.js
+++ src/components/Wings.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react'
+import { Jumbotron } from 'react-bootstrap'
 
 export default class Wings extends Component {
   render () {
     return <div>
       <h1 className='heads1'>Wings</h1>
+      <main className='mainB'>
+        <Jumbotron className='jumbo1' />
+      </main>
     </div>
   }
 }
