5ee5813de367962560fb82b9e3df6de4bb1b223c
Brian Bycynski <high5bri@hotmail.com>
2017-02-13 06:35:09 -0500

    minor changes

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import { PageHeader } from 'react-bootstrap'
 import Thumbnails from './Thumbnails'
+import { Button, Menu } from 'semantic-ui-react'
+import { Link } from 'react-router'
 
 export default class Layout extends Component {
   static propTypes = {
@@ -9,6 +11,9 @@ export default class Layout extends Component {
   render () {
     return <div>
       <PageHeader className='head'><p>Rest-au-rant</p></PageHeader>
+      <Button size='huge' className='homeBut'>
+        <Menu.Item as={Link} to='/'>Home</Menu.Item>
+      </Button>
       <Thumbnails />
       <main>
         { this.props.children }
