eabd439a2e7426298d3732acad48c5c21bbc9a01
Brian Bycynski <high5bri@hotmail.com>
2017-02-14 11:28:26 -0500

    minor changes

--- src/components/Seafood.js
+++ src/components/Seafood.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react'
-import { Jumbotron } from 'react-bootstrap'
+import { ListGroup, ListGroupItem } from 'react-bootstrap'
 
 export default class Seafood extends Component {
   render () {
     return <div>
       <h1 className='heads1'>Seafood</h1>
       <main className='mainB'>
-        <Jumbotron className='jumbo1' />
+        {/* <Jumbotron className='jumbo1' /> */}
+        <ListGroup className='seaFoodList'>
+          <ListGroupItem header='Casual Clam' href='http://www.casualclam.com/' />
+        </ListGroup>
       </main>
     </div>
   }
