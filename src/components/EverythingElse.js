4f4d2d20826bc5c8194da3e4c8ed705404b878c0
Brian Bycynski <high5bri@hotmail.com>
2017-02-16 12:18:44 -0500

    minor changes

--- src/components/EverythingElse.js
+++ src/components/EverythingElse.js
@@ -5,8 +5,9 @@ export default class EverythingElse extends Component {
     return <div>
       <h1 className='heads1'>Everything Else</h1>
       <main>
-        <ListGroup>
+        <ListGroup bsClass='lists'>
           <ListGroupItem header='Bjs BrewHouse' href='https://www.bjsrestaurants.com' />
+          <ListGroupItem header='Pollo Tropical' href='http://pollotropical.com/home-page' />
         </ListGroup>
       </main>
     </div>
