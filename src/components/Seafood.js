42d29ddae8fb5741e76fda1a05fb17b3701c44f6
Brian Bycynski <high5bri@hotmail.com>
2017-02-16 09:51:11 -0500

    added Everything Else category

--- src/components/Seafood.js
+++ src/components/Seafood.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react'
 import { ListGroup, ListGroupItem } from 'react-bootstrap'
-
 export default class Seafood extends Component {
   render () {
     return <div>
       <h1 className='heads1'>Seafood</h1>
       <main>
-        <ListGroup>
+        <ListGroup className='lists'>
           <ListGroupItem header='Casual Clam' href='http://www.casualclam.com/' />
         </ListGroup>
       </main>
