1002b8599c1b8514795b285a21af59abffb686a3
Brian Bycynski <high5bri@hotmail.com>
2017-02-17 09:05:17 -0500

    made some changes to font-size

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react'
 import {PageHeader} from 'react-bootstrap'
 import Thumbnails from './Thumbnails'
-import {Button, Menu} from 'semantic-ui-react'
+import {Menu} from 'semantic-ui-react'
 import {Link} from 'react-router'
 export default class Layout extends Component {
   static propTypes = {
@@ -11,9 +11,9 @@ export default class Layout extends Component {
     return <div>
       <PageHeader className='head'>
         {/* <p>Rest-au-rant</p> */}
-        <Button size='huge'>
-          <Menu.Item as={Link} to='/'><p>Rest-au-rant</p></Menu.Item>
-        </Button>
+        {/* <Button size='huge'> */}
+        <Menu.Item className='menu1'as={Link} to='/'><p>Rest-au-rant</p></Menu.Item>
+        {/* </Button> */}
       </PageHeader>
       <Thumbnails />
       <main>
--- src/components/Seafood.js
+++ src/components/Seafood.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { ListGroup, ListGroupItem } from 'react-bootstrap'
+import { Button, Row, Col, Icon, Table } from 'react-materialize'
 export default class Seafood extends Component {
   render () {
     return <div>
@@ -8,6 +9,38 @@ export default class Seafood extends Component {
         <ListGroup className='lists'>
           <ListGroupItem header='Casual Clam' href='http://www.casualclam.com' />
         </ListGroup>
+        <Row>
+          <Col s={6}>
+            <Button waves='light'>Add<Icon right>add</Icon></Button>
+          </Col>
+          <Col s={6}>
+            <Button waves='light'>Remove<Icon right>remove</Icon></Button>
+          </Col>
+        </Row>
+        <Table bordered centered>
+          <tbody>
+            <tr className='menu1'>
+              <td href='http://www.casualclam.com'>Casual Clam</td>
+              <td>Eclair</td>
+              {/* <td>$0.87</td> */}
+            </tr>
+            <tr>
+              <td>Alan</td>
+              <td>Jellybean</td>
+              <td>$3.76</td>
+            </tr>
+            <tr>
+              <td>Jonathan</td>
+              <td>Lollipop</td>
+              <td>$7.00</td>
+            </tr>
+          </tbody>
+        </Table>
+        {/* <Col m={6} s={12}>
+          <Card className='blue-grey darken-1' textClassName='white-text' title='Card title' actions={[<a href='#'>This is a link</a>]}>
+    I am a very simple card.
+    </Card>
+        </Col> */}
       </main>
     </div>
   }
