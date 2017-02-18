bb362e89559b1ee67372e6c4281ea85bd17c5a50
Brian Bycynski <high5bri@hotmail.com>
2017-02-18 07:53:59 -0500

    minor change

--- src/components/EverythingElse.js
+++ src/components/EverythingElse.js
@@ -2,7 +2,11 @@ import React, { Component } from 'react'
 import { ListGroup, ListGroupItem } from 'react-bootstrap'
 // import { Embed } from 'semantic-ui-react'
 export default class EverythingElse extends Component {
+
   render () {
+    function alertClicked () {
+      alert('Hello Bdog')
+    }
     return <div>
       <h1 className='heads1'>Everything Else</h1>
       <main>
@@ -21,7 +25,7 @@ export default class EverythingElse extends Component {
           <ListGroupItem header='Pollo Tropical' href='http://pollotropical.com/home-page' />
           <ListGroupItem header='Glory Days' href='https://www.glorydaysgrill.com' />
           <ListGroupItem header='Cheddars' href='http://cheddars.com' />
-          <ListGroupItem header='Pei Wei' href='https://www.peiwei.com' />
+          <ListGroupItem onClick={alertClicked} header='Pei Wei' href='https://www.peiwei.com' />
         </ListGroup>
       </main>
     </div>
